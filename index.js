8692f6f073f4a6b6812424211e0a2a260ff48ab6
Gulthor <jbmusso+github@gmail.com>
2013-10-15 16:16:04 +0200

    Add placeholder methods

---
+++ index.js
@@ -0,0 +1,7 @@
+var plugin;
+
+plugin = require("./src/plugin");
+
+module.exports = {
+  plugin: plugin
+};
---
+++ src/plugin.js
@@ -0,0 +1,15 @@
+module.exports = function(schema, options) {
+  schema.method("getInbox", function(callback) {
+    return callback();
+  });
+
+
+  schema.method("hasConversationRequestFrom", function(user) {
+    
+  });
+
+
+  schema.method("canConverseWith", function(user) {
+
+  });
+};
